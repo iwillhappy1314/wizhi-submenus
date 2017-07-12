cd5c41c6ca0e0a205e10331b8178b8d2ff89e936
李二庆（Amos Lee） <iwillhappy1314@gmail.com>
2017-07-12 16:09:48 +0800

    添加排除分类和关联页面替代分类的方法

--- sub-menus.php
+++ sub-menus.php
@@ -3,7 +3,7 @@
 	<?php
 	if ( is_page() ) {
 
-		global $post;
+		$post = get_queried_object();
 
 		if ( $post->post_parent ) {
 			$children = wp_list_pages( "title_li=&child_of=" . $post->post_parent . "&echo=0" );
@@ -15,46 +15,78 @@
 
 			echo $args[ 'before_title' ] . get_the_title( $post->post_parent ) . $args[ 'after_title' ]; ?>
 
-			<ul>
+            <ul>
 				<?php echo $children; ?>
-			</ul>
+            </ul>
 
 		<?php }
 
 	} else {
 
-		$taxonomy = '';
+		$taxonomy        = '';
+		$related_page_id = false;
+		$queried_object  = get_queried_object();
+		$post_type       = $queried_object->name;
 
-		if ( is_single() ) {
+		if ( is_single() || is_singular() ) {
 
-			global $post;
-			$queried_object = get_queried_object();
-			$post_type      = $post->post_type;
-			$taxonomies     = array_values( get_object_taxonomies( $post_type, 'objects' ) );
+			$post_type  = $queried_object->post_type;
+			$taxonomies = array_values( get_object_taxonomies( $post_type, 'objects' ) );
 
-			$taxonomy = $taxonomies[ 0 ]->name;
+			// 获取文章的分类方法
+			$terms     = wp_get_post_terms( $queried_object->ID, $taxonomies[ 0 ]->name );
+			$main_term = $terms[ 0 ];
+
+			// 获取分类法关联的页面
+			$related_page_id = get_term_meta( $main_term->term_id, '_related_page', true );
+			$related_page    = get_post( $related_page_id );
+
+			if ( $related_page_id ) {
+
+				// 父级页面为分类关联的父级页面的页面 ID
+				$children = wp_list_pages( "title_li=&child_of=" . $related_page->post_parent . "&echo=0" );
+
+				if ( $children ) {
+					echo $args[ 'before_title' ] . get_the_title( $related_page->post_parent ) . $args[ 'after_title' ]; ?>
+
+                    <ul>
+						<?php echo $children; ?>
+                    </ul>
+				<?php }
+
+			} else {
+				$taxonomy = $taxonomies[ 0 ]->name;
+			}
 
 		} elseif ( is_post_type_archive() ) {
 
-			$queried_object = get_queried_object();
-			$post_type      = $queried_object->name;
-			$taxonomies     = array_values( get_object_taxonomies( $post_type, 'objects' ) );
-
-			$taxonomy = $taxonomies[ 0 ]->name;
+			$taxonomies = array_values( get_object_taxonomies( $post_type, 'objects' ) );
+			$taxonomy   = $taxonomies[ 0 ]->name;
 
 		} else {
-
-			$queried_object = get_queried_object();
-			$taxonomy       = $queried_object->taxonomy;
+			$taxonomy = $queried_object->taxonomy;
 
 			if ( $taxonomy == 'post_tag' ) {
 				$taxonomy = null;
 			}
 
+			$terms = get_terms( [
+				'taxonomy'   => $taxonomy,
+				'hide_empty' => false,
+			] );
+
+			// 获取有关键的分类目录并排除
+			$exclude = collect( $terms )->filter( function ( $term ) {
+				return get_term_meta( $term->term_id, '_related_page', true );
+			} )->flatten()->toArray();
+
+			$exclude = $exclude[0]->term_id;
+
 		}
 
 		$args_tax = [
 			'taxonomy'     => $taxonomy,
+			'exclude'      => $exclude,
 			'show_count'   => 0,
 			'pad_counts'   => 0,
 			'hierarchical' => 0,
@@ -65,16 +97,20 @@
 		$post_type_obj   = get_post_type_object( $post_type );
 		$post_type_lable = $post_type_obj->labels->singular_name;
 
-		if ( ! is_tag() ) {
+		if ( ! is_tag() && ! $related_page_id ) {
 			echo $args[ 'before_title' ] . $post_type_lable . $args[ 'after_title' ];
 		}
 
-		?>
+		if ( ! $related_page_id ) {
 
-		<ul>
-			<?php wp_list_categories( $args_tax ); ?>
-		</ul>
+			?>
 
-	<?php } //end is_page ?>
+            <ul>
+				<?php wp_list_categories( $args_tax ); ?>
+            </ul>
+
+		<?php }
+
+	} //end is_page ?>
 
 <?php } //end !is_home ?>
